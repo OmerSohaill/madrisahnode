a4781dd9a87ee9a8dc5b6758b4455243d9748ac1
mianumer116 <128652547+mianumer116@users.noreply.github.com>
2024-05-06 02:23:06 +0500

    WORKING ON FRONT END

---
+++ nodejs/server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+
+const path = require('path');
+const cors = require('cors');
+const app = express();
+const port = 3001;
+// Middleware
+app.use(cors());
+
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+//MongoDB connection
+const mongoURI = 'mongodb+srv://umer:umer@cluster0.avg1bjf.mongodb.net/railway?retryWrites=true&w=majority';
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true });
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB');
+});
+
+
+app.get('/newvoteradd', function(req, res) {
+  res.render('uploadimg')
+
+})
+
+// Start the server
+app.listen(port, () => {
+  console.log('Server is listening on Port', port);
+});
